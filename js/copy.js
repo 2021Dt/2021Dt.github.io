2bb62ad39a42dd7c623488a2184be68a7df37517
2021Dt <1538415560@qq.com>
2024-01-17 17:37:31 +0800

    Site updated: 2024-01-17 17:37:30

---
+++ js/copy.js
@@ -0,0 +1,18 @@
+// 复制提醒
+document.addEventListener("copy", function () {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "复制成功了~(￣▽￣)~*",
+                    message: "记得好评哦！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 5000
+                });
+            }
+        })
+    }, 300);
+})
---
+++ js/title.js
@@ -0,0 +1,17 @@
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener("visibilitychange", function () {
+  if (document.hidden) {
+    //离开当前页面时标签显示内容
+    document.title = "这就走了？";
+    clearTimeout(titleTime);
+  } else {
+    //返回当前页面时标签显示内容
+    document.title = "抓到你了" + OriginTitile;
+    //两秒后变回正常标题
+    titleTime = setTimeout(function () {
+      document.title = OriginTitile;
+    }, 2000);
+  }
+});
